b2b1260f22a4d03bcdd8ba9800e899c1a79a8c8c
Văn Hiệp <dhiep2307@gmail.com>
2025-02-18 08:24:19 +0700

    update header no cache

--- src/service/AxiosCofig.js
+++ src/service/AxiosCofig.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    withCredentials: true,
-    baseURL: process.env.REACT_APP_API_DOMAIN
+  withCredentials: true,
+  baseURL: process.env.REACT_APP_API_DOMAIN,
+  headers: {
+    "Cache-Control": "no-cache, no-store, must-revalidate",
+    Pragma: "no-cache",
+    Expires: "0",
+  },
 });
 
-export { axiosInstance }
+export { axiosInstance };
